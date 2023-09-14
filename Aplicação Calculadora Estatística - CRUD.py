077c51e23e6d793a4d838f7af6bdc7320ba26c70
Eduardobin <eduardobin.eng@gmail.com>
2023-09-14 19:24:42 -0300

    Update Aplicação Calculadora Estatística - CRUD.py

--- Aplicação Calculadora Estatística - CRUD.py
+++ Aplicação Calculadora Estatística - CRUD.py
@@ -3,7 +3,8 @@ import math
 #Entrada de dados!!
 #Import para teste
 lista = [["mercado",1000],["agua, luz e net",500],["aluguel",1500],["lazer",600],["cartão de crédito",1200]]
-#Processamento Pareto
+lista2 = [100,101,100,99,95,90,99,96,98,102,124,112,106,108,91,90,90,96,93,102,98,103,103,93,93,92]
+#Processamento Pareto [str,num]
 def pareto(lista):
     lr=[]
     soma=0
@@ -47,6 +48,57 @@ def ordenando (lista):
         L=len(lista)
     print('lista ordenada = ', listord)
     return listord
+#Medidas e tabelas de distribuição de frequência
+def dist_freq(lista):
+    lista.sort()
+    tamanho = len(lista)
+    media = sum(lista)/len(lista)
+    maximo = max(lista)
+    minimo = min(lista)
+    amplitude = maximo-minimo
+    if tamanho%2 == 0:
+        mediana = (lista[(tamanho//2)-1]+lista[(tamanho//2)])/2
+        quartis1 = (lista[((tamanho//2)//2)-1]+lista[((tamanho//2)//2)])/2
+        quartis3 = (lista[(tamanho*3//4)]+lista[(tamanho*3//4)+1])/2
+    else:
+        mediana = lista[(tamanho//2)-1]
+        quartis1 = lista[((tamanho//2)//2)-1]
+        quartis3 = lista[(tamanho*3//4)+1]
+    iqr = quartis3-quartis1
+    outup = quartis3 + 1.5*iqr
+    outdown = quartis1 - 1.5*iqr
+    dados_out = []
+    for i in lista:
+        if i > outup or i < outdown:
+            dados_out.append(i)
+#moda autoral
+    moda=[]
+    x=0
+    y=0
+    for i in lista:
+        x=lista.count(i)
+        if x > y:
+            moda=[]
+            y=x
+            moda.append(i)
+        elif x == y:
+            if i not in moda:
+                moda.append(i)
+    print(media)
+    print(maximo)
+    print(minimo)
+    print(amplitude)
+    print(quartis1)
+    print(mediana)
+    print(quartis3)
+    print(lista)
+    print('dados fora: ',dados_out)
+    print(moda)
 
 
-print(f'{pareto(ordenando(lista))}')
+dist_freq(lista2)
+
+
+
+
+
